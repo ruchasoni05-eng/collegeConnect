1818de0d2bc9add6750dc3a3dcb5c70d2fc17060
ruchasoni05-eng <ruchasoni05@gmail.com>
2026-04-23 22:52:41 +0530

    added dotenv cofiguration and updated package files, removed test files

--- test_mongo.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ruchasoni05_db_user:Ruch%4074_%21@cluster0.z1vtjgg.mongodb.net/college-feedback?appName=Cluster0', { family: 4 })
-  .then(()=>console.log('Connected'))
-  .catch(e=>console.log(e.message));
--- test_mongo2.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://ruchasoni05_db_user:Ruch%4074_%21@ac-a1c6hyv-shard-00-00.z1vtjgg.mongodb.net:27017,ac-a1c6hyv-shard-00-01.z1vtjgg.mongodb.net:27017,ac-a1c6hyv-shard-00-02.z1vtjgg.mongodb.net:27017/college-feedback?ssl=true&replicaSet=atlas-a1c6hyv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
-  .then(()=>console.log('Connected'))
-  .catch(e=>console.log(e.message));
